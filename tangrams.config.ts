2aa8ae36f2c09c84195f08fcde978ebc11273acf
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-12-11 23:03:23 -0600

    refactor: migrate to tangrams package

---
+++ tangrams.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "tangrams";
+
+export default defineConfig({
+  sources: [
+    {
+      name: "github",
+      type: "graphql",
+      schema: {
+        url: "https://api.github.com/graphql",
+        headers: {
+          Authorization: `token ${process.env.GITHUB_TOKEN}`,
+        },
+      },
+      scalars: {
+        URI: "string",
+        DateTime: "string",
+        HTML: "string",
+        GitObjectID: "string",
+        GitSSHRemote: "string",
+        Base64String: "string",
+        PreciseDateTime: "string",
+        X509Certificate: "string",
+      },
+      documents: "./src/graphql/**/*.graphql",
+      generates: ["query"],
+    },
+  ],
+});
